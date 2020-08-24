569d6b86e19d6c57c7f99719b9a0ce26ffa9903d
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2020-08-24 19:39:05 +0530

    Update readfiles.py

--- readfiles.py
+++ readfiles.py
@@ -1,21 +1,38 @@
 import os
+from textblob import TextBlob
 
 path = r'C:\Users\param\Desktop\Daily_logs'
 d = 1
-while d < 20:
+while d < 24:
     try:
         date = str(d) + ".8.2020.txt"
         completepath = os.path.join(path, date)
         f = open(completepath,'r')
-        massage = f.readlines()
+        massage = f.read()
+        #print(massage)
         f.close()
-        
-        f1 = open(os.path.join(path,"demofile3.txt"), "a")
-        f1.close()
-        print(massage)            
-        
+        f1 = open("demofile3.txt", "a")
+        f1.write(massage)
+        f1.close()            
         d += 1
     except: 
         pass
         d += 1
+
+f2 = open("demofile3.txt","r")
+pullData = f2.read()
+dataArray = pullData.split('\n')
+text = '.'
+for eachline in dataArray:
+    if len(eachline)>1:
+        text= text + eachline
+blob = TextBlob(text)
+
+print(blob.sentences)
+for sentence in blob.sentences:
+    print(sentence)
+    print("\n")
+    print(sentence.sentiment)
+    print("\n")
+
     
